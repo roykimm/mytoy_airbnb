a5bc2668916cc21429ae82457eb2619da561011c
kimmyunghoon <roykimmyunghoon@gmail.com>
2022-01-23 23:37:04 +0900

    day 4 during

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-14 py-14 bg-gray-100">
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+            <p>how dfd</p>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
